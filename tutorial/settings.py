27e40ce276b4fa294e5818122c4c2544412dd561
Koshi <k_shi_615@MSI.localdomain>
2025-10-22 10:23:34 +0900

    djangoでスタートアップする

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h0)r=49ff4k+r7_-^f)8h)t$)!s)p=-!l!aarwy(q08_+rgsh2'
+SECRET_KEY = 'django-insecure-xazo3f4^6!f8bw(m!sngoe99)vi9q@tcj6nevzfbp^iyyx6b&5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
