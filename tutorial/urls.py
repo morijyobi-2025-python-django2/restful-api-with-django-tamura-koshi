0469f9c3ad4a281111f9d198a9238142a87c7106
Koshi <k_shi_615@MSI.localdomain>
2025-10-30 12:17:32 +0900

    feat: Configure API URLs using router

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -14,7 +14,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers
+
+from tutorial.quickstart import views
+
+# 1. ルーターを作成
+router = routers.DefaultRouter()
+# 2. ViewSet をルーターに登録
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# 3. URLパターンを設定
+urlpatterns = [
+    # routerが自動生成したURL (e.g., /users/, /groups/) を読み込む
+    path('', include(router.urls)),
+
+    # DRFの「ログイン/ログアウト」機能（ブラウザブルAPI用）
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
