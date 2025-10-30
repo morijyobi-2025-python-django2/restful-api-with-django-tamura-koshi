2f05e9908bcb2017a994c833edc0754d7c61a666
Koshi <k_shi_615@MSI.localdomain>
2025-10-30 12:39:13 +0900

    fix: Use specific AppConfig for quickstart in INSTALLED_APPS

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'tutorial.quickstart',
+    'tutorial.quickstart.apps.QuickstartConfig',
 ]
 
 MIDDLEWARE = [
