74cae074aa2e22f5046090bfea74372e02e256f8
Koshi <k_shi_615@MSI.localdomain>
2025-11-12 10:52:27 +0900

    feat: Add Syllabus model and migration

--- syllabus/models.py
+++ syllabus/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Syllabus(models.Model):
+    """
+    シラバスのモデル
+    """
+    # (openapi.yaml に合わせたフィールド定義)
+    title_ja = models.CharField(max_length=100)
+    title_en = models.CharField(max_length=100, blank=True)
+    teacher_name = models.CharField(max_length=100)
+    school_year = models.IntegerField()
+    semester = models.CharField(max_length=20)
+    num_credits = models.IntegerField()
+    target_grade = models.CharField(max_length=20)
+    course_type = models.CharField(max_length=50)
+
+    overview_ja = models.TextField(blank=True)
+    overview_en = models.TextField(blank=True)
+    prerequisites_ja = models.TextField(blank=True)
+    prerequisites_en = models.TextField(blank=True)
+
+    textbook = models.TextField(blank=True)
+    reference_book = models.TextField(blank=True)
+
+    grading_policy_ja = models.TextField(blank=True)
+    grading_policy_en = models.TextField(blank=True)
+
+    notes_ja = models.TextField(blank=True)
+    notes_en = models.TextField(blank=True)
+
+    # (自動設定されるフィールド)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title_ja
