c7a95206fc7a301a03f9431c925182079a050e88
Koshi <k_shi_615@MSI.localdomain>
2025-11-12 10:57:06 +0900

    feat: Register Syllabus model to admin

--- syllabus/admin.py
+++ syllabus/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Syllabus  # .models から Syllabus モデルをインポート
 
+# Syllabus モデルを管理サイトに登録
+admin.site.register(Syllabus)
 # Register your models here.
