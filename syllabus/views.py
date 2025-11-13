79ad4ba281cfa92dba72488661784e34a49d62de
Koshi <k_shi_615@MSI.localdomain>
2025-11-13 11:34:43 +0900

    feat: Add IsAuthenticatedOrReadOnly permission to SyllabusViewSet

--- syllabus/views.py
+++ syllabus/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework import permissions  # <-- 1. この行を追加
 from .models import Syllabus
 from .serializers import SyllabusSerializer
 
@@ -6,5 +7,8 @@ class SyllabusViewSet(viewsets.ModelViewSet):
     """
     シラバスのAPI (一覧, 作成, 取得, 更新, 削除)
     """
-    queryset = Syllabus.objects.all() # どのデータを使うか (Syllabusの全データ)
-    serializer_class = SyllabusSerializer # どの翻訳機(Serializer)を使うか
+    queryset = Syllabus.objects.all()
+    serializer_class = SyllabusSerializer
+
+    # 権限設定を追加: 読み取りは誰でもOK, 書き込みは認証ユーザーのみ
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # <-- 2. この行を追加
