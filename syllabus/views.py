aaa5269a3a5bf52341c78d7f3a40db6163675ce0
Koshi <k_shi_615@MSI.localdomain>
2025-11-13 11:44:31 +0900

    feat: Add pagination and filtering to Syllabus API

--- syllabus/views.py
+++ syllabus/views.py
@@ -1,14 +1,11 @@
-from rest_framework import viewsets
-from rest_framework import permissions  # <-- 1. この行を追加
+from rest_framework import viewsets, permissions
 from .models import Syllabus
 from .serializers import SyllabusSerializer
 
 class SyllabusViewSet(viewsets.ModelViewSet):
-    """
-    シラバスのAPI (一覧, 作成, 取得, 更新, 削除)
-    """
     queryset = Syllabus.objects.all()
     serializer_class = SyllabusSerializer
-
-    # 権限設定を追加: 読み取りは誰でもOK, 書き込みは認証ユーザーのみ
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # <-- 2. この行を追加
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
+    # 検索対象とするフィールドを指定
+    filterset_fields = ['school_year', 'semester', 'course_type', 'teacher_name'] # <-- この行を追加
--- tutorial/settings.py
+++ tutorial/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'tutorial.quickstart.apps.QuickstartConfig',
     'syllabus.apps.SyllabusConfig',
 ]
@@ -123,3 +124,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# (ファイルの一番下)
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    
+    # フィルターのバックエンドとして django-filter を指定
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
+}
