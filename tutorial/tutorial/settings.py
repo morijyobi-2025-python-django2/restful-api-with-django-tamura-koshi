e0361847005675c3c7e79b35c8da7ea373552c8a
Koshi <k_shi_615@MSI.localdomain>
2025-12-17 10:03:04 +0900

    Add comprehensive OpenAPI specification for Syllabus API
    - Define Syllabus schema with all fields from memo-api.txt
      - Subject info: name, regulation name, teacher
      - Course details: instructor type, method, sessions count
      - Enrollment: classification, grade, departments
      - Content: overview, objectives, grading criteria
      - Resources: textbook, certification, costs
    - Add ClassSession schema for コマシラバス
    - Implement RESTful endpoints: GET, POST, PUT, PATCH, DELETE
    - Configure authentication: Basic Auth and Session Auth
    - Add pagination support for list endpoint
    - Set up Django REST framework quickstart example
    - Install djangorestframework package

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +116,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
